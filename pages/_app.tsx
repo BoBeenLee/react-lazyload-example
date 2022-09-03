be9c435c15342f55f18be33f889ed1f6def90b08
bobeenlee <globaldev@naver.com>
2022-09-03 22:12:45 +0900

    feat: add react-query config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../libs/query-client";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface PageProps {
+  dehydratedState: unknown;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  const { dehydratedState } = pageProps as PageProps;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
